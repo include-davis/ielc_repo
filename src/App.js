3126b4018452a513e39a4ced4c3e3412496c0e7c
yugii28 <yugandhar28@hotmail.com>
2023-03-06 20:20:36 -0800

    made file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import AboutUs from "./Pages/AboutUs";
+import ContactUs from "./Pages/ContactUs";
+import GradeOne from "./Pages/GradeOne";
+import GradeTwo from "./Pages/GradeTwo";
+import GradeThree from "./Pages/GradeThree";
+import GradeFour from "./Pages/GradeFour";
+import GradeFive from "./Pages/GradeFive";
+import HomePage from "./Pages/HomePage";
+import Register from "./Pages/Register";
+import Volunteer from "./Pages/Volunteer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path = "/" element = {<HomePage/>}></Route>
+        <Route path = "/" element = {<AboutUs/>}></Route>
+        <Route path = "/" element = {<ContactUs/>}></Route>
+        <Route path = "/" element = {<GradeOne/>}></Route>
+        <Route path = "/" element = {<GradeTwo/>}></Route>
+        <Route path = "/" element = {<GradeThree/>}></Route>
+        <Route path = "/" element = {<GradeFour/>}></Route>
+        <Route path = "/" element = {<GradeFive/>}></Route>
+        <Route path = "/" element = {<Register/>}></Route>
+        <Route path = "/" element = {<Volunteer/>}></Route>
+      </Routes>
     </div>
   );
 }
