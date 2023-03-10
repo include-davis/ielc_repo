3db61f12d42f1ce8e3aa0639415f7f0f60488df1
Austin Shih <austinhshih@gmail.com>
2023-03-10 11:24:05 -0800

    Finished part 1 and 2 of demo

---
+++ src/Components/JS/DemoModal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "../CSS/DemoModal.css"
+
+export default function DemoModal(props) {
+    const { buttonInfo } = props;
+    return (
+        <div className='modal-container'>
+            <button className='exit-button'>x</button>
+            <h2>MENU</h2>
+            <div className='buttons'>
+                {
+                    buttonInfo.map((button, index) => {
+                        return (
+                            <button 
+                                className='auto-button' 
+                                key={index}
+                                onClick={button.onClick}>
+                                    <p className='p-small'>{button.name}</p>
+                            </button>)
+                    })
+                }
+                <div className='movement-buttons'>
+                    <button>{`<--`}</button>
+                    <button>{`-->`}</button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/Pages/JS/Components.js
+++ src/Pages/JS/Components.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import "../CSS/Components.css"
+import DemoModal from '../../Components/JS/DemoModal';
 
+const buttons = [
+    {
+        name: 'button 1',
+        onClick: () => {alert(`clicked button 1!`)},
+    },
+    {
+        name: 'button 2',
+        onClick: () => {alert(`clicked button 2!`)},
+    },    
+    {
+        name: 'button 3',
+        onClick: () => {alert(`clicked button 3!`)},
+    }
+]
 export default function Components() {
     return (
         <div className='container'>
             <h1>Components</h1>
-            
+            <DemoModal buttonInfo={buttons}></DemoModal>
         </div>
     )
 }
